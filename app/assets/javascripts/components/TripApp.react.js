cbc9217d75bbfb3aee48ef3a64967e98f615fe60
Matthew Tejo <matthew.tejo@gmail.com>
2014-11-16 22:16:06 -0500

    added react and class

---
+++ app/assets/javascripts/components/TripApp.react.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var TripApp = React.createClass({
+
+	render: function(){
+		return(
+			<h2>REact Works</h2>
+			);
+	}
+});
+
+module.exports= TripApp;
--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,1 +1,12 @@
-alert('hello');
+var React = require('react');
+
+var TripApp = require('./components/TripApp.react');
+
+$(document).ready(function () {
+
+React.render(
+		<TripApp />,
+        document.getElementById('app')
+      );
+
+});
